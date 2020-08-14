d2f6eceaf4bc581b7549298b93d0981194fd9eac
roukell <62136980+roukell@users.noreply.github.com>
2020-08-14 22:16:42 +1000

    updated functional files

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require("../app/connection.js");
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
 
-// Creates a "Burger" model that matches up with DB
-class Burger extends Model {}
-Burger.init({
-    author: DataTypes.STRING,
-    body: DataTypes.STRING,
-    devour: {type: DataTypes.BOOLEAN, defaultValue: false}
-}, { sequelize, modelName: 'burger'})
+const burger = {
+  all: (cb) => {
+    orm.all("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: (cols, vals, cb) => {
+    orm.create("burgers", cols, vals, (res) => {
+      cb(res);
+    });
+  },
+  update: (objColVals, condition, cb) => {
+    orm.update("burgers", objColVals, condition, (res) => {
+      cb(res);
+    });
+  },
+  delete: (condition, cb) => {
+    orm.delete("burgers", condition, (res) => {
+      cb(res);
+    });
+  }
+};
 
-// Syncs with DB
-Burger.sync();
-
-// Makes the Burger Model available for other files (will also create a table)
-module.exports = Burger;
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
-const db = require("./models/burger");
-const app = express();
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./public"));
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./routes/api-routes.js");
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgerController.js");
+
 app.use(routes);
 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });

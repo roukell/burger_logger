40ae8e43ab69400784383ccfb537ece8e564fa21
roukell <62136980+roukell@users.noreply.github.com>
2020-08-16 00:28:56 +1000

    set timeout attempted to fix heroku H13 error

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
+const timeout = require('connect-timeout')
 
 const app = express();
+
+// Timeout
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -21,8 +30,11 @@ const routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
+
 // Start our server so that it can begin listening to client requests.
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
